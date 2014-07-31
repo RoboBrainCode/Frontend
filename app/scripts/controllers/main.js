950bc7abab07ac64b162e934ea9e145311bc2654
Kevin Lee <kevinlee5893@gmail.com>
2014-07-31 12:58:51 -0400

    restyled feeds

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,6 +6,8 @@
  * @description
  * # MainCtrl
  * Controller of the roboBrainApp
+ * Note: Feed type must be one of: affordance, vision, planning, object-usage, 3D-detection
+ * Media content type must be one of: image
  */
 angular.module('roboBrainApp')
   .controller('MainCtrl', function ($scope) {
