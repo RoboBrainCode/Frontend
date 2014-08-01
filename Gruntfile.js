e5145b9cc854062f4c783d40be548860467bb8fb
Kevin Lee <kevinlee5893@gmail.com>
2014-08-01 12:37:13 -0400

    removed all static file renaming, third attempt at fixing contacts page

--- Gruntfile.js
+++ Gruntfile.js
@@ -210,7 +210,7 @@ module.exports = function (grunt) {
         src: [
           '<%= yeoman.dist %>/scripts/{,*/}*.js',
           '<%= yeoman.dist %>/styles/{,*/}*.css',
-          '<%= yeoman.dist %>/images/*.{png,jpg,jpeg,gif,webp,svg}',
+          '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
           '<%= yeoman.dist %>/styles/fonts/*'
         ]
       }
@@ -432,7 +432,6 @@ module.exports = function (grunt) {
     'cdnify',
     'cssmin',
     'uglify',
-    'filerev',
     'usemin',
     'htmlmin'
   ]);
