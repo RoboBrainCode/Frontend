d7609e7cf33523b3c81ecc834844ce0a2471b0d3
Debarghya Das <dd367@cornell.edu>
2014-09-24 07:57:16 -0400

    fab

--- fabfile.py
+++ fabfile.py
@@ -3,7 +3,7 @@ from fabric.api import cd, env, local, settings, run, sudo
 from fabric.colors import green, red
 from fabric.contrib.console import confirm
 
-def prod_deploy(speed='fast'):
+def prod_deploy(user='ubuntu',speed='fast'):
   print(red('Deploying Frontend to production at robobrain.me...'))
   if not confirm('Are you sure you want to deploy to production?'):
      print(red('Aborting deploy.'))
@@ -30,7 +30,7 @@ def prod_deploy(speed='fast'):
       sudo('grunt build')
   print(red('Done!'))
 
-def test_deploy(speed='fast'):
+def test_deploy(user='ubuntu', speed='fast'):
   env.host_string = 'ec2-54-218-20-10.us-west-2.compute.amazonaws.com'
   env.key_filename = 'conf/www.pem'
   env.user = user
