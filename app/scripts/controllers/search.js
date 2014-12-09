7bc17e5982296abfa16a8b21d0190adb1600d3eb
deedydas <dd367@cornell.edu>
2014-12-09 08:34:21 +0000

    Minor change. Replaced previous angularjs query with ajax jquery

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -8,22 +8,31 @@
  * Controller to query the knowledge graph
  */
 angular.module('roboBrainApp')
-  .controller('SearchCtrl', ['$scope', '$http', function ($scope, $http, ENV) {
+  .config(function($httpProvider){
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+  })
+  /*.config('SearchCtrl',[]).config(function($sceDelegateProvider) {
+                $sceDelegateProvider.resourceUrlWhitelist([
+                // Allow same origin resource loads.
+                     'self',
+                // Allow loading from our assets domain.  Notice the difference between * and **.
+                    'http://graphtest.robobrain.me:3000/graph_query/nlquery/**'
+                                  ]);})*/
+  .controller('SearchCtrl', ['$scope', '$http', function ($scope, $http,  ENV) {
 
-    var search_endpoint = "http://graph.robobrain.me:3000/graph_query/nlquery/"
+    var search_endpoint = "http://graphtest.robobrain.me:3000/graph_query/nlquery/";
     $scope.hasSearched = false;
     $scope.hasAnswered = false;
     $scope.hasError = false;
-
     $scope.searchGraph = function(){
         $scope.hasAnswered = false;
         $scope.hasError = false;
         $scope.hasSearched = true;
-
+       // $httpProvider.defaults.withCredentials = true;
         var input = $scope.inputText;
         $scope.inputText = "";
         $scope.originalText = input;
-
+        alert("Intput is "+input);
         $http.get(search_endpoint, {
             params: {
                 user_in: input,
@@ -31,12 +40,15 @@ angular.module('roboBrainApp')
         }).success(function(data, status) {
             //console.log("SUCCESS");
             //console.log(data);
+            alert("Success");
+            //alert(data);
             $scope.outputText = data[0].user_out;
             $scope.hasSearched = false;
             $scope.hasAnswered = true;
         })
         .error(function(data, status) {
             //console.log("ERROR");
+            alert("Error "+status+" "+data);
             $scope.hasSearched = false;
             $scope.hasError = true;
         });
