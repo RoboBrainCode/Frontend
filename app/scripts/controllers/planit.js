11c56828b4bbd38afebe8f6c60e3a0037386e1ad
SiddhantManocha <siddhantmanocha1994@gmail.com>
2015-06-17 18:42:27 -0400

    added planit

--- app/scripts/controllers/planit.js
+++ app/scripts/controllers/planit.js
@@ -4,7 +4,9 @@ angular.module('roboBrainApp')
         
 
     $scope.activity_name='dancing';
-    $rootScope.dispWarn=""
+    $rootScope.dispWarn="";
+    $rootScope.daeFile="";
+    $rootScope.xmlFile="";
     $scope.loadMore=function()
     {
         document.getElementById('seemore').style.display='block';
@@ -27,16 +29,37 @@ angular.module('roboBrainApp')
             $("#wheel").removeClass("spinning");
             var newImage = new Image();
             newImage.src = data['result'];
+            console.log(data['result']);
             document.getElementById('heatMapResult').innerHTML='<img src="'+data['result']+'" alt="Heat Map" title="Heat Map" />';
 
 
         });
 
+    }
+    $scope.getTrajectory=function()
+    {
+        console.log('getting trajectory');
+        if ($rootScope.daeFile=="" || $rootScope.xmlFile=="")
+        {
+            alert('Upload Files before proceeding');
+            return;
+        }
+        var planitEndPoint="http://localhost:6363/planit/getTraj/"
+        $.getJSON(planitEndPoint,
+        {
+            daeFile:$rootScope.daeFile,
+            xmlFile:$rootScope.xmlFile
+        }, 
+        function(data) 
+        {
+            console.log(data);
+            document.getElementById('trajResult').innerHTML='<a href="'+data['result']+'">Click here to download the trajectory </a>';
 
+            
 
+        });
 
     }
-
 
         var uploader = $scope.uploader = new FileUploader({
             url: 'http://localhost:6363/planit/uploadFile/'
@@ -47,7 +70,7 @@ angular.module('roboBrainApp')
         uploader.filters.push({
             name: 'customFilter',
             fn: function(item /*{File|FileLikeObject}*/, options) {
-                return this.queue.length < 10;
+                return this.queue.length < 2;
             }
         });
 
@@ -57,6 +80,53 @@ angular.module('roboBrainApp')
             console.info('onWhenAddingFileFailed', item, filter, options);
         };
         uploader.onAfterAddingFile = function(fileItem) {
+            var filename=fileItem._file.name;
+            var extension=filename.split('.').pop();
+            var arr=['xml','dae'];
+            
+            // console.log(filename,extension);
+            if (arr.indexOf(extension)>-1)
+            {
+                console.info('Allowed');
+                if ($scope.uploader.queue.length==2)
+                {
+                    console.log($scope.uploader.queue);
+                    
+                    if (($scope.uploader.queue[0]._file.name.split('.').pop()=="xml" && extension=='dae') || ($scope.uploader.queue[0]._file.name.split('.').pop()=="dae" && extension=='xml') )
+                    {
+                        if (extension=='dae')
+                        {
+                            $rootScope.daeFile=filename;
+                            $rootScope.xmlFile=$scope.uploader.queue[0]._file.name;
+                        }
+                        else
+                        {
+                            $rootScope.xmlFile=filename;
+                            $rootScope.daeFile=$scope.uploader.queue[0]._file.name;
+                        }
+                    }
+                    else
+                    {
+                        alert('One of the files should be .dae and other .xml');
+                        $scope.uploader.queue=[];
+                    }
+                }
+            }
+            else
+            {
+                console.log('Not allowed');
+                alert('Only dae and xml files are allowed for upload');
+                for (var i=0;i<$scope.uploader.queue.length;i++)
+                {
+                    if ($scope.uploader.queue[i]._file.name==filename);
+                    {
+                        $scope.uploader.queue.splice(i,1);
+                        break;
+                    }
+                }
+                console.log($scope.uploader.queue);
+            }
+            // console.log();
             console.info('onAfterAddingFile', fileItem);
         };
         uploader.onAfterAddingAll = function(addedFileItems) {
